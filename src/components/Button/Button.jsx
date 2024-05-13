9c26a3057984bdbc58caed4265096bd44f9b0401
Alina <alinkartemenko@mail.ru>
2024-05-13 22:16:21 +0300

    corrected comments

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,3 @@
-import styles from "./Button.module.css";
-
-const Button = ({ text }) => {
-  return <button className={styles.button}>{text}</button>;
-};
-
-export default Button;
+export default function Button({ text }) {
+  return <button>{text}</button>;
+}
